e50bc9fb98f7f682d626435d3aef364f213ffe91
Maksim Leskin <maksim.leskin@methed.ru>
2023-01-29 15:09:06 +0300

    day4

--- js/timer.js
+++ js/timer.js
@@ -1,6 +1,7 @@
 import { alarm } from './alarm.js';
 import { changeActiveBtn } from './control.js';
 import { state } from './state.js';
+import { showTodo, updateTodo } from './todo.js';
 import { addZero } from './util.js';
 
 const minutesElem = document.querySelector('.time__minutes');
@@ -11,19 +12,33 @@ export const showTime = (seconds) => {
   secondsElem.textContent = addZero(seconds % 60);
 }
 
+const title = document.title;
+
 export const startTimer = () => {
-  state.timeLeft -= 1;
+  const countdown = new Date().getTime() + state.timeLeft * 1000;
+  console.log('countdown: ', countdown);
+  
+  state.timerId = setInterval(() => {
+    state.timeLeft -= 1;
+    showTime(state.timeLeft);
 
-  showTime(state.timeLeft);
+    document.title = state.timeLeft;
 
-  if (state.timeLeft > 0 && state.isActive) {
-    state.timerId = setTimeout(startTimer, 1000);
-  }
+    if (!(state.timeLeft % 10)) {
+      const now = new Date().getTime();
+      state.timeLeft = Math.floor((countdown - now) / 1000);
+    }
 
-  if (state.timeLeft <= 0) {
+    if (state.timeLeft > 0 && state.isActive) {
+      return;
+    }
+
+    document.title = title;
+
     if (state.status === 'work') {
       state.activeTodo.pomodoro += 1;
-
+      updateTodo(state.activeTodo);
+      
       if (state.activeTodo.pomodoro % state.count) {
         state.status = 'break'
       } else {
@@ -36,6 +51,7 @@ export const startTimer = () => {
     alarm();
     state.timeLeft = state[state.status] * 60;
     changeActiveBtn(state.status);
+    showTodo();
     startTimer();
-  }
+  }, 1000)
 }
