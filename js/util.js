b8959d3a450e05f36fb559a8f93bbb48e4207a6a
Maksim Leskin <maksim.leskin@methed.ru>
2023-02-03 20:49:04 +0300

    fix bags

--- js/util.js
+++ js/util.js
@@ -1,1 +1,13 @@
-export const addZero = n => n < 10 ? '0' + n : n;
+/**
+ * Добавляет ноль в начало если число с одной цифрой
+ * @param (number)
+ * @throw (typeof number)
+ * @return (string)
+*/
+
+export const addZero = n => {
+  if (isNaN(parseInt(n) && isFinite(n))) {
+    throw new Error('Параметр не являеться числом')
+  }
+  return n < 10 ? '0' + n : n
+};
